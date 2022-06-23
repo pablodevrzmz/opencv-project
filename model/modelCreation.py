c019c785792157f3bd43809ffc9cbfd3bab7ad7a
Adrian Alvarez <adrian.alvarezrodriguez@ucr.ac.cr>
2022-06-23 12:53:14 -0600

    Adding the model

--- model/modelCreation.py
+++ model/modelCreation.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2 as cv
 import glob
+import os
+import matplotlib.pyplot as plt
 from tensorflow.keras.applications import DenseNet121 as DN
 from tensorflow.keras import Sequential, metrics
 from tensorflow.keras.models import Model, load_model
@@ -8,26 +10,24 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, GlobalAveragePooling2D, Flatten, Dropout, BatchNormalization
 
-#Labels: 0=rock, 1=paper, 2=scissors
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+#Labels: [1,0,0]=rock, [0,1,0]=paper, [0,0,1]=scissors
 def loadImages(path, label):
     images = glob.glob(path+'/*.png')
     images_data = []
     labels = []
-    if label==1:
-        labels = np.ones(len(images))
-    else:
-        labels = np.zeros(len(images))
     #Loading Images and creating label array
     for i in range(0, len(images)):
-        if (label != 0 and label != 1):
-            labels[i] = label
+        labels.append(label)
         img = cv.imread(images[i])
-        images_data.append(img)
-    return images_data, labels
+        images_data.append(img/255)
+    return images_data, np.array(labels)
         
 
 def loadDataset(processingMethod, dataChunkToLoad):
     paths = []
+    labels = [[1,0,0], [0,1,0], [0,0,1]]
     if dataChunkToLoad=='train':
         paths = ['./datasets/'+processingMethod+'/train/rock', './datasets/'+processingMethod+'/train/paper', './datasets/'+processingMethod+'/train/scissors']
     elif dataChunkToLoad=='test':
@@ -40,12 +40,13 @@ def loadDataset(processingMethod, dataChunkToLoad):
     y = []
     X_len = 0
     for i in range(0,len(paths)):
-        X_partial, y_partial = loadImages(paths[i], i)
+        X_partial, y_partial = loadImages(paths[i], labels[i])
         X_len = len(X_partial) + X_len
         X = np.append(X, X_partial)
         y = np.append(y, y_partial)
     
     X = X.reshape(X_len, 300, 300, 3)
+    y = y.reshape(X_len, 3)
     # Shuffling dataset, X and y items share the same index
     shuffledIndexes = np.random.permutation(len(y))
     X = X[shuffledIndexes]
@@ -54,10 +55,42 @@ def loadDataset(processingMethod, dataChunkToLoad):
     return X, y
 
 
-def createModel(processingMethod='Unprocessed'):
+def createModel(processingMethod='Unprocessed', model_index=0):
     X_train, y_train = loadDataset(processingMethod, 'train')
     X_test, y_test = loadDataset(processingMethod, 'test')
-    print(y_train.shape)
-    print(X_train.shape)
 
-    #This is where the model work start
+    model_name=processingMethod+str(model_index)
+    #Creating the DenseNet model
+    #References: https://www.pluralsight.com/guides/introduction-to-densenet-with-tensorflow
+    #            https://bouzouitina-hamdi.medium.com/transfer-learning-with-keras-using-densenet121-fffc6bb0c233
+    denseArchitecture = DN(weights="imagenet", include_top=False, input_shape=(300, 300, 3))
+    #https://www.tensorflow.org/api_docs/python/tf/keras/layers/Layer
+    for layer in denseArchitecture.layers[:121]:
+        layer.trainable = False #we are using imagenet, so this layers shouldn't be trainable
+
+    #input = denseArchitecture.input
+    #added_layers = MaxPooling2D()(denseArchitecture.output)
+    #added_layers = BatchNormalization()(added_layers)
+    #added_layers = Dense(512, activation='relu')(added_layers)
+    #added_layers = Dropout(0.33)(added_layers)
+    #added_layers = Flatten()(added_layers)
+    #added_layers = Dense(64, activation='relu')(added_layers)
+    #added_layers = Dense(3, activation='softmax')(added_layers)
+    #model = Model(input, added_layers)
+    model = Sequential() #DenseNet is a sequential architecture
+    model.add(denseArchitecture)
+    model.add(MaxPooling2D())
+    model.add(BatchNormalization())
+    model.add(Dropout(0.33))
+    #Activations: https://www.tensorflow.org/api_docs/python/tf/keras/activations
+    model.add(Flatten())
+    model.add(Dense(512, activation="relu"))
+    model.add(Dense(3, activation="softmax"))
+    #Reference: https://machinelearningmastery.com/adam-optimization-algorithm-for-deep-learning/
+    model.compile(optimizer=Adam(learning_rate=0.001), metrics=['accuracy', metrics.Precision(name='precision'), metrics.Recall(name='recall')], loss='categorical_crossentropy')
+    model_metrics = model.fit(x=X_train, y=y_train, epochs=10, validation_data=(X_test, y_test))
+    model.save(model_name)
+    model.evaluate(x=X_test, y=y_test)
+    plt.plot(model_metrics.history['accuracy'], color='blue', label='train')
+    plt.plot(model_metrics.history['val_accuracy'], color='orange', label='test')
+    plt.show()
